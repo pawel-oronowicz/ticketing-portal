03ebc5543688a3aaf7718eb86460da76a9c359f1
Paweloronowicz <oronowicz@gmail.com>
2026-08-23 11:11:44 +0200

    Add check whether Telescope class exists

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local')) {
+        if ($this->app->environment('local') && class_exists(TelescopeServiceProvider::class)) {
             $this->app->register(TelescopeServiceProvider::class);
         }
     }
