c0db80605402679e6de397aa17953fe8da982c3f
Paweloronowicz <oronowicz@gmail.com>
2026-08-23 13:38:58 +0200

    Fix default Admin User demo seeding

--- api/database/seeders/DemoSeeder.php
+++ api/database/seeders/DemoSeeder.php
@@ -6,6 +6,7 @@ use App\Enums\UserRole;
 use App\Models\User;
 use Hash;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DemoSeeder extends Seeder
 {
@@ -15,9 +16,11 @@ class DemoSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'username' => 'Demo Admin',
+            'name' => 'Demo Admin',
             'email' => 'demoadmin@ticketingportal.com',
+            'email_verified_at' => now(),
             'password' => Hash::make('demo1234'),
+            'remember_token' => Str::random(10),
             'role' => UserRole::Admin,
         ]);
 
